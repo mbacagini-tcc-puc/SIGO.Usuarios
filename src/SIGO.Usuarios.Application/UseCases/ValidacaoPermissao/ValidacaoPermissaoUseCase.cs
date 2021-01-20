38edd5fc548b03bd2b56ab27e414fc7149a3d6a6
Murilo Eduardo Bacagini Santos <msantos@viceri.com.br>
2021-01-19 22:13:39 -0300

    Caso de uso de validação de permissão

---
+++ src/SIGO.Usuarios.Application/UseCases/ValidacaoPermissao/ValidacaoPermissaoUseCase.cs
@@ -0,0 +1,23 @@
+﻿using SIGO.Usuarios.Application.Repositories;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGO.Usuarios.Application.UseCases.ValidacaoPermissao
+{
+    public class ValidacaoPermissaoUseCase : IValidacaoPermissaoUseCase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public ValidacaoPermissaoUseCase(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task<bool> ValidarPermissao(int usuarioId, string modulo)
+        {
+            var usuario = await _usuarioRepository.ObterUsuarioPorId(usuarioId);
+
+            return usuario.Modulos.Any(moduloPermitido => moduloPermitido.Nome == modulo);
+        }
+    }
+}
---
+++ tests/SIGO.Usuarios.Test.Application/ValidacaoPermissaoUseCaseTests.cs
@@ -0,0 +1,69 @@
+﻿using NSubstitute;
+using SIGO.Usuarios.Application.Repositories;
+using SIGO.Usuarios.Application.UseCases.ValidacaoPermissao;
+using SIGO.Usuarios.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SIGO.Usuarios.Test.Application
+{
+    public class ValidacaoPermissaoUseCaseTests
+    {
+        private readonly ValidacaoPermissaoUseCase _validacaoPermissaoUseCase;
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        private const int UsuarioId = 1;
+        private const string Modulo = "consultorias-assessorias";
+
+        public ValidacaoPermissaoUseCaseTests()
+        {
+            _usuarioRepository = Substitute.For<IUsuarioRepository>();
+            _validacaoPermissaoUseCase = new ValidacaoPermissaoUseCase(_usuarioRepository);
+        }
+
+        [Fact]
+        public async Task DeveRetornarPermissao()
+        {
+            // arrange
+            var usuario = new Usuario
+            {
+                Id = UsuarioId,
+                Modulos = new List<Modulo>
+                 {
+                     new Modulo { Nome = Modulo }
+                 }
+            };
+
+            _usuarioRepository.ObterUsuarioPorId(UsuarioId).Returns(usuario);
+
+            // act
+            var resultado = await _validacaoPermissaoUseCase.ValidarPermissao(UsuarioId, Modulo);
+
+            // Assert
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public async Task NaoDeveRetornarPermissao()
+        {
+            // arrange
+            var usuario = new Usuario
+            {
+                Id = UsuarioId,
+                Modulos = new List<Modulo>
+                 {
+                     new Modulo { Nome = "outro-modulo" }
+                 }
+            };
+
+            _usuarioRepository.ObterUsuarioPorId(UsuarioId).Returns(usuario);
+
+            // act
+            var resultado = await _validacaoPermissaoUseCase.ValidarPermissao(UsuarioId, Modulo);
+
+            // Assert
+            Assert.False(resultado);
+        }
+    }
+}
