55b5927c804571d4c3f1bb7dcd2ad1e34cbdfe72
Murilo Eduardo Bacagini Santos <msantos@viceri.com.br>
2021-01-22 12:46:24 -0300

    Validação de permissão

--- src/SIGO.Usuarios.API/Controllers/ValidacaoPermissaoController.cs
+++ src/SIGO.Usuarios.API/Controllers/ValidacaoPermissaoController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SIGO.Usuarios.Application.Services;
+using SIGO.Usuarios.Application.UseCases.ValidacaoPermissao;
+using System.Threading.Tasks;
 
 namespace SIGO.Usuarios.API.Controllers
 {
@@ -13,17 +10,24 @@ namespace SIGO.Usuarios.API.Controllers
     [Authorize("Bearer")]
     public class ValidacaoPermissaoController : ControllerBase
     {
+        private readonly IValidacaoPermissaoUseCase _validacaoPermissaoUseCase;
         private readonly IUsuarioAutenticadoService _usuarioAutenticadoService;
 
-        public ValidacaoPermissaoController(IUsuarioAutenticadoService usuarioAutenticadoService)
+        public ValidacaoPermissaoController(IUsuarioAutenticadoService usuarioAutenticadoService, IValidacaoPermissaoUseCase validacaoPermissaoUseCase)
         {
             _usuarioAutenticadoService = usuarioAutenticadoService;
+            _validacaoPermissaoUseCase = validacaoPermissaoUseCase;
         }
 
         [Route("auth/permissoes")]
-        public IActionResult ValidarPermissao([FromQuery] string modulo)
+        public async Task< IActionResult> ValidarPermissao([FromQuery] string modulo)
         {
-            var usuario = _usuarioAutenticadoService.Usuario;
+            var permitido = await _validacaoPermissaoUseCase.ValidarPermissao(_usuarioAutenticadoService.Usuario.Id, modulo);
+
+            if(!permitido)
+            {
+                return Forbid();
+            }
 
             return Ok();
         }
---
+++ tests/SIGO.Usuarios.Test.Api/ValidacaoPermissaoControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using SIGO.Usuarios.API.Controllers;
+using SIGO.Usuarios.Application.Services;
+using SIGO.Usuarios.Application.TransferObjects;
+using SIGO.Usuarios.Application.UseCases.ValidacaoPermissao;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SIGO.Usuarios.Test.Api
+{
+    public class ValidacaoPermissaoControllerTests
+    {
+        private readonly ValidacaoPermissaoController _validacaoPermissaoController;
+        private readonly IValidacaoPermissaoUseCase _validacaoPermissaoUseCase;
+        private readonly IUsuarioAutenticadoService _usuarioAutenticadoService;
+
+        private const int UsuarioId = 1;
+        private const string Modulo = "usuarios";
+
+        public ValidacaoPermissaoControllerTests()
+        {
+            _validacaoPermissaoUseCase = Substitute.For<IValidacaoPermissaoUseCase>();
+            _usuarioAutenticadoService = Substitute.For<IUsuarioAutenticadoService>();
+            _validacaoPermissaoController = new ValidacaoPermissaoController(_usuarioAutenticadoService, _validacaoPermissaoUseCase);
+            _usuarioAutenticadoService.Usuario.Returns(new UsuarioAutenticado
+            {
+                 Email = "teste@teste.com.br",
+                 Id = UsuarioId
+            });
+        }
+
+        [Fact]
+        public async Task DeveRetornarForbid()
+        {
+            // arrange
+            _validacaoPermissaoUseCase.ValidarPermissao(UsuarioId, Modulo).Returns(false);
+
+            // act
+            var resultado = await _validacaoPermissaoController.ValidarPermissao(Modulo);
+
+            // assert
+            Assert.IsType<ForbidResult>(resultado);
+        }
+
+        [Fact]
+        public async Task DeveRetornarOk()
+        {
+            // arrange
+            _validacaoPermissaoUseCase.ValidarPermissao(UsuarioId, Modulo).Returns(true);
+
+            // act
+            var resultado = await _validacaoPermissaoController.ValidarPermissao(Modulo);
+
+            // assert
+            Assert.IsType<OkResult>(resultado);
+        }
+    }
+}
