4410c645ad05e4b5bfd4d8db9cb3f5bf5c4c1074
Murilo Eduardo Bacagini Santos <msantos@viceri.com.br>
2021-01-27 10:47:15 -0300

    Health check

---
+++ src/SIGO.Usuarios.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SIGO.Usuarios.API.Controllers
+{
+    [Route("health")]
+    [ApiController]
+    [AllowAnonymous]
+    public class HealthCheckController : ControllerBase
+    {
+        public IActionResult Check()
+        {
+            return Ok();
+        }
+    }
+}
